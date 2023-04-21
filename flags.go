9e7500a7c6342504d34eaa266589f240f4ee887c
Shubham Pathak <20816337+shubhampathak@users.noreply.github.com>
2023-04-21 22:03:50 +0530

    feat: add flags for adding input

---
+++ flags.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+type args struct {
+	domain      string // target domain name
+	list        string // filepath
+	environment string // environment
+}
+
+func parseFlags() args {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-d domain] [-l list] [-e environment]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	domain := flag.String("d", "", "Target domain")
+	list := flag.String("l", "", "Full Path to list of sub-domains that have same origin IP address as of target domain")
+	environment := flag.String("e", "", "Environment against which you want to test. Available options: staging or production")
+
+	flag.Parse()
+
+	if len(*domain) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	hostNames, err := net.LookupHost(*domain)
+	if err != nil || len(hostNames) == 0 {
+		fmt.Fprintf(color.Output, "%s %s\n", color.RedString("[Error]"), "Invalid input! Please provide a valid domain.")
+		os.Exit(1)
+	} else {
+		fmt.Fprintf(color.Output, "%s\n", color.BlueString("Target Domain: ")+*domain)
+	}
+
+	var allowedEnvironment string
+	if *environment == "staging" || *environment == "production" {
+		allowedEnvironment = *environment
+		fmt.Fprintf(color.Output, "%s\n", color.BlueString("Target Environment: ")+allowedEnvironment)
+	}
+
+	return args{
+		domain:      *domain,
+		list:        *list,
+		environment: allowedEnvironment,
+	}
+}
