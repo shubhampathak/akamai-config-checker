61ed3519d83e794f9f9190990e3162e57cb90fa5
Shubham Pathak <20816337+shubhampathak@users.noreply.github.com>
2025-05-01 02:26:42 +0530

    refactor: parsing logic

--- flags.go
+++ flags.go
@@ -28,30 +28,38 @@ func parseFlags() args {
 
 	flag.Parse()
 
+	// Ensure at least one of domain or list is provided
 	if len(*domain) == 0 && len(*list) == 0 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
+	// Validate the domain if provided
 	if len(*domain) > 0 {
-		if validDomain(*domain) {
-			fmt.Fprintf(color.Output, "%s\n", color.BlueString("Target Domain: ")+*domain)
-		} else {
-			fmt.Fprintf(color.Output, "%s %s\n", color.RedString("[Error]"), "Invalid input! Please provide a valid domain.")
+		_, err := isValidDomainAndBehindAkamai(*domain)
+		if err != nil {
+			fmt.Fprintf(color.Output, "%s %s %v\n", color.RedString("[Error]"), "Domain validation failed:", err)
 			os.Exit(1)
 		}
+		fmt.Fprintf(color.Output, "%s\n", color.BlueString("Target Domain: ")+*domain)
 	}
 
-	var allowedEnvironment string
-	if *environment == "staging" || *environment == "production" {
-		allowedEnvironment = *environment
-		fmt.Fprintf(color.Output, "%s\n", color.BlueString("Target Environment: ")+allowedEnvironment)
+	// Validate the environment
+	selectedEnvironment := "staging"
+	if len(*environment) > 0 {
+		if *environment == "staging" || *environment == "production" {
+			selectedEnvironment = *environment
+			fmt.Fprintf(color.Output, "%s\n", color.BlueString("Target Environment: ")+selectedEnvironment)
+		} else {
+			fmt.Fprintf(color.Output, "%s %s\n", color.RedString("[Error]"), "Invalid environment! Available options are: staging or production.")
+			os.Exit(1)
+		}
 	}
 
 	return args{
 		domain:      *domain,
 		list:        *list,
-		environment: allowedEnvironment,
+		environment: selectedEnvironment,
 		backup:      *backup,
 	}
 }
